e25ef620bf07488ecc3f460771f54058ade4f5b5
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2020-12-11 18:20:15 -0300

    exercicio_9

---
+++ Exercício 9/readme.mod
@@ -0,0 +1,9 @@
+Declare uma arrowFunction que deverá possuir dois parâmetros (nome, idade).
+A nossa função deve mostrar no console a seguinte frase:
+“Olá, eu sou
+<nome>
+	, e tenho
+	<idade>
+		anos”.
+	</idade>
+</nome>
---
+++ Exercício 9/script/scrit.js
@@ -0,0 +1,4 @@
+function nomeEIdade(nome, idade) {
+    const pessoa = (nome, idade) => { console.log(`“Olá, eu sou ${nome}, e tenho ${idade} anos”.`) }
+    document.getElementById("resultado").value = `“Olá, eu sou ${nome}, e tenho ${idade} anos”.`
+}
